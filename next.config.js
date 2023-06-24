10114e38f4a60d9b71756939c744130a1aaf8010
leonardohra-gh <leonardohra.gh@gmail.com>
2023-06-24 14:03:38 -0300

    Trying to fix the problem with images on GH Pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+// if we are using github pages, we need to set up the images on public folder
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = 
 {
     // This is necessary for github pages, github actions, it seems
     output: 'export',
-    // Images are not being found on github pages, this might solve it
-    images: {
-        loader: "akamai",
-        path: "/",
-    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
 module.exports = nextConfig
