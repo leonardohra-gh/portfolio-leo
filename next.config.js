8bfc4f9ebe39853a866ea0f5b3485f719c5bf398
leonardohra-gh <leonardohra.gh@gmail.com>
2023-06-24 14:11:27 -0300

    Removing github pages support

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-// if we are using github pages, we need to set up the images on public folder
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-const nextConfig = 
-{
-    // This is necessary for github pages, github actions, it seems
-    output: 'export',
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
