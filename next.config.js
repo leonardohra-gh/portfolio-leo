4002f4d5e0d9d4f704b000e8f8342b8c98cefb4d
leonardohra-gh <leonardohra.gh@gmail.com>
2023-06-24 13:54:52 -0300

    Trying to fix images not showing up in GH pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = 
 {
     // This is necessary for github pages, github actions, it seems
-    output: 'export'
+    output: 'export',
+    // Images are not being found on github pages, this might solve it
+    images: {
+        loader: "akamai",
+        path: "/",
+    },
 }
 
 module.exports = nextConfig
