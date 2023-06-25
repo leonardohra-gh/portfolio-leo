d4ddd13f5a475443ebc5823089bfdd67ebb72825
leonardohra-gh <leonardohra.gh@gmail.com>
2023-06-25 00:20:23 -0300

    Most of navigation bar done

---
+++ app/components/navbar.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import Link from "next/link";
+import { Disclosure } from "@headlessui/react";
+import Image from "next/image";
+
+const MenuSVG = (params: {"open": boolean}) =>
+{
+    // Draw a sandwich menu if the menu isn't open, otherwise draw an X
+    return(
+        <svg
+            className="w-6 h-6 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24">
+            {
+                params.open && 
+                (
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                    />
+                )
+            }
+            {
+                !params.open && 
+                (
+                    <path
+                        fillRule="evenodd"
+                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                    />
+                )
+            }
+        </svg>
+    )
+}
+
+const NavigationBar = () =>
+{
+    const navigation = 
+    [
+        // TODO change content to match language
+        ["Home", "/"],
+        ["Projects", "/projects"],
+        ["About me", "/about-me"],
+        // TODO Blog?
+    ]
+
+    return(
+        <div className="w-full">
+            <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+                {/* Logo  */}
+                <Disclosure>
+                    {
+                        ({open}) =>
+                        (
+                            <>
+                                <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+                                    <Link href="/">
+                                        <span className="flex items-center space-x-2 text-2x1 font-medium text-indigo-500 dark:text-gray-100">
+                                            <span>
+                                                {/* TODO Check if it is possible to change Image when in another mode  */}
+                                                <Image 
+                                                    src="/logo.png"
+                                                    alt="Neural network resembling a brain"
+                                                    width="32"
+                                                    height="32"
+                                                    className="w-8 dark:invert"
+                                                />
+                                            </span>
+                                            {/* TODO Change content to match language  */}
+                                            <span>Portfolio</span>
+                                        </span>
+                                    </Link>
+
+                                    <Disclosure.Button
+                                        aria-label="Toggle Menu"
+                                        className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
+                                        <MenuSVG open={open} />
+                                    </Disclosure.Button>
+
+                                    <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
+                                        <>
+                                            {
+                                                navigation.map
+                                                (
+                                                    (item, index) =>
+                                                    (
+                                                        <Link key={index} href={item[1]} className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
+                                                            {item[0]}
+                                                        </Link>
+                                                    ) 
+                                                )
+                                            }
+                                        </>
+                                    </Disclosure.Panel>
+                                </div>
+                            </>
+                        )
+                    }
+                </Disclosure>
+
+                {/* menu  */}
+                <div className="hidden text-center lg:flex lg:items-center">
+                    <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+                        { 
+                            navigation.map
+                            (
+                                (menu, index) =>
+                                (
+                                    <li className="mr-3 nav__item" key={index}>
+                                        <Link href={menu[1]} className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                                            {menu[0]}
+                                        </Link>
+                                    </li>
+                                )
+                            ) 
+                        }
+                    </ul>
+                </div>
+                
+                <div className="hidden mr-3 space-x-4 lg:flex nav__item">
+                    
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavigationBar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        trueGray: colors.neutral,
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        stock: [defaultTheme.fontFamily.sans],
       },
     },
   },
