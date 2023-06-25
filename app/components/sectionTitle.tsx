022c00ad7aebca2ebddb2cbcf4b1c6abe6e3c78e
leonardohra-gh <leonardohra.gh@gmail.com>
2023-06-25 20:07:08 -0300

    Home page almost done!

---
+++ app/components/sectionTitle.tsx
@@ -0,0 +1,50 @@
+import Container from '@/app/components/container'
+
+const SectionTitle = 
+(
+    {
+        align,
+        pretitle,
+        title,
+        children
+    }:
+    {
+        align?: string,
+        pretitle?: string,
+        title: string,
+        children: React.ReactNode
+    }
+) =>
+{
+    return(
+        <Container
+            className={`flex w-full flex-col mt-4
+                ${
+                    align === 'left' ? "" : "items-center justify-center text-center"
+                }`}
+            >
+            {pretitle && 
+            (
+                <div className='text-sm font-bold tracking-wider text-indigo-600 uppercase'>
+                    {pretitle}
+                </div>
+            )}
+
+            {title && 
+            (
+                <h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
+                    {title}
+                </h2>
+            )}
+
+            {children &&
+            (
+                <div className='max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300 text-justify'>
+                    {children}
+                </div>
+            )}
+        </Container>
+    );
+}
+
+export default SectionTitle;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
-export default function Home() {
+import SectionTitle from "@/app/components/sectionTitle";
+import Checklist from "@/app/components/checklist";
+
+export default function Home() 
+{
+  const goals = 
+  [
+    ["Creating my home page", true],
+    ["Creating a navigation bar", true],
+    ["Creating the other pages (About me, Projects, Contact me)", false],
+    ["Adding multi-language support (English, Brazilian Portuguese, Spanish)", false],
+    ["Creating the details page for each project", false],
+    ["Improving SEO", false],
+  ]
+
   return (
     <main>
+      <SectionTitle
+        title="What can you find here?">
+        <p>
+          In this website you will be able to learn a bit more about me and my professional work and skills.
+          <br/><br/>
+          In "Projects", you can see the projects I have worked on. 
+          You can see sample projects that I have created while learning a framework/library/language.
+          There are also my personal projects, which are focused on creating something functional.
+          And finally there are projects I have developed for clients. 
+          <br/><br/>
+          In "About me", I talk a bit about my academic progress and the jobs I have worked on. 
+          I also summarize my skills in different areas, it is complementar to my curriculum.
+        </p>
+      </SectionTitle>
+      <SectionTitle
+        title="Development">
+        <p>
+          This website is one of my personal projects, so I will talk about it in depth in the "Projects" section, 
+          but there are a few goals I am still working on, while developing this website.<br/>
+          By the end of the project, I intend to complete the following goals:<br/>
+        </p>
+        <Checklist content={goals}/>
+      </SectionTitle>
+      
     </main>
   )
 }
