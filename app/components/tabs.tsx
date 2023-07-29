3770574b3cb0e5c6dcb8f848ed03e9d61134d896
leonardohra-gh <leonardohra.gh@gmail.com>
2023-07-29 10:06:12 -0300

    Added Modular card for the tabs

--- app/components/tabs.tsx
+++ app/components/tabs.tsx
@@ -5,6 +5,14 @@ import { Tabs } from 'flowbite-react';
 import Image from "next/image";
 import office from "public/projects/office.jpg";
 
+type StaticImageData = 
+{ 
+    src: string; 
+    height: number; 
+    width: number; 
+    blurDataURL?: string; 
+}
+
 const InteractiveTabs = 
 (
     {
@@ -18,11 +26,17 @@ const InteractiveTabs =
     }
 ) =>
 {
+
+    const cardTempImg = office
+    const cardTempTitle = "Noteworthy technology acquisitions 2021"
+    const cardTempDesc = "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
+    const cardTempLink = "#"
     return (
-        <div id='tabs' className='mx-auto md:w-1/2'>
+        <div className='mx-auto md:w-1/2'>
             <Tabs.Group
                 aria-label='Projects tab'
-                style='underline'>
+                style='underline'
+                className='mx-2'>
                     {tabTitles.map
                     ( 
                         (
@@ -33,7 +47,8 @@ const InteractiveTabs =
                                 <Tabs.Item
                                     active
                                     title={value}>
-                                        <p>{tabDescription[index]}</p>
+                                        <h2 className='my-2'>{tabDescription[index]}</h2>
+                                        <TabCard cardImage={cardTempImg} cardTitle={cardTempTitle} cardDescription={cardTempDesc} link={cardTempLink} />
                                 </Tabs.Item>
                             )
                         }
@@ -41,6 +56,39 @@ const InteractiveTabs =
                 
             </Tabs.Group>
         </div>
+    )
+}
+
+const TabCard = 
+(
+    {
+        cardImage,
+        cardTitle,
+        cardDescription,
+        link,
+    }
+    :
+    {
+        cardImage: StaticImageData,
+        cardTitle: string,
+        cardDescription: string,
+        link: string
+    }
+) => 
+{
+    return (
+        <a 
+            href={link} 
+            className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <Image 
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" 
+                src={cardImage} 
+                alt=""/>
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{cardTitle}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{cardDescription}</p>
+            </div>
+        </a>
     )
 }
 
