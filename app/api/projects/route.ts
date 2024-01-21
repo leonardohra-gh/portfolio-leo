a1b1024bca03ea29a1edce0872db300482aa86c2
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-01-21 18:16:00 -0300

    Descartando o api routes

--- app/api/projects/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import dataTabs from "@/public/data_json/data_tabs.json"
-
-export const GET = async(req: Request, res: Response) =>
-{
-  return NextResponse.json(dataTabs);
-}
-
