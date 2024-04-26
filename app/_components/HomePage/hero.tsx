9581896a4d23928c638253b137664fa3160e5eb6
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-04-26 16:11:51 -0300

    Addings translation to the hero component

--- app/_components/HomePage/hero.tsx
+++ app/_components/HomePage/hero.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 import { Button } from "flowbite-react";
 import ViewOnGithub from "@/app/_components/General/viewOnGithub";
 import Link from "next/link";
-import Container from "@/app/_components/General/container";
 import { SecondLevelHeading } from "@/app/_components/General/typography";
 import { useTranslation } from "@/app/i18n/client";
 
-export default function ProfileCard({lng}: {lng:string}) {
-
+export default function ProfileCard({lng}: {lng:string}) 
+{
+  const {t} = useTranslation(lng, 'hero')
   return (
     <div className="flex flex-col items-center">
       <Image
@@ -20,15 +20,14 @@ export default function ProfileCard({lng}: {lng:string}) {
         className="mb-3 rounded-full shadow-lg w-60 h-60"
       />
       <SecondLevelHeading
-        title="Meu Portfólio"
-        subtitles= {[ "Sou um desenvolvedor Full Stack, que gosta de aprender novas tecnologias e ferramentas, sempre se desafiando.", 
-        "Acredito que mais importante do que saber tudo, é ser capaz de aprender o que não sei de forma eficiente."]}
+        title={t("title")}
+        subtitles= {[t("sub-title-1"), t("sub-title-2")]}
         classNamePlus="text-center"
       />
       <div className="mx-auto flex flex-col md:flex-row items-center gap-1 md:p-5 md:gap-5">
-        <Button color="purple"><Link href={`/${lng}/projects`}>Conheça meus projetos</Link></Button>
-        <Button color="purple">Entre em contato</Button>
-        <ViewOnGithub text="Veja meu repositório no Github" url="https://github.com/leonardohra-gh?tab=repositories" />
+        <Button color="purple"><Link href={`/${lng}/projects`}>{t("button-1")}</Link></Button>
+        <Button color="purple">{t("button-2")}</Button>
+        <ViewOnGithub text={t("button-3")} url="https://github.com/leonardohra-gh?tab=repositories" />
       </div>
     </div>
   );
