9b262b04590e0a04998cf808422ba14ee7bb4c8b
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-04-30 22:10:42 -0300

    Added testimonials element to home page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "@/app/i18n";
 import Hero from "@/app/_components/HomePage/hero" ;
 import { ContactForm } from "@/app/_components/HomePage/contactForm";
 import { CarouselImageAndList, CarouselImageAndListItem } from "@/app/_components/General/carousel";
+import { TestimonialsElement, Testimonial } from "../_components/HomePage/testimonials";
 
 export default async function Home
 (
@@ -14,15 +15,19 @@ export default async function Home
   }
 ) 
 {
-  const {t} = await useTranslation(Props.params.lng, 'skill-carousel')
-  const items: CarouselImageAndListItem[] = t("items", { returnObjects: true })
-  const carouselTitle = t("title")
-  const carouselSubtitle = t("subtitle")
-
+  const t_carousel = await useTranslation(Props.params.lng, 'skill-carousel')
+  const items: CarouselImageAndListItem[] = t_carousel.t("items", { returnObjects: true })
+  const carouselTitle = t_carousel.t("title")
+  const carouselSubtitle = t_carousel.t("subtitle")
+  const t_testimonials = await useTranslation(Props.params.lng, 'home/testimonials')
+  const testTitle = t_testimonials.t("title")
+  const testSubtitle: string[] = t_testimonials.t("subtitles", { returnObjects: true })
+  const testItems: Testimonial[] = t_testimonials.t("testimonials", { returnObjects: true })
   return (
     <main>
       <Hero lng={Props.params.lng}/>
       <CarouselImageAndList carouselTitle={carouselTitle} carouselSubtitle={carouselSubtitle} items={items}/>
+      <TestimonialsElement title={testTitle} subtitles={testSubtitle} peopleTestimonial={testItems} />
       <ContactForm lng={Props.params.lng}/>
     </main>
   )
