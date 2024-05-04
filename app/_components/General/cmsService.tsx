e55c94a6e2b795ace540ff597e611459bbe96380
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-05-04 12:19:03 -0300

    Testing revalidate to check if the pages get new content for projects every 6 min

--- app/_components/General/cmsService.tsx
+++ app/_components/General/cmsService.tsx
@@ -2,55 +2,59 @@ import { CMSResultCategory, CMSResultProject, LocalizedValue } from "@/app/types
 
 export function getLocalizedText(localizedArray: LocalizedValue[], lng: string)
 {
-    const cmsLangs: {[cmsLang: string]: string} = 
-    {
-        "en": "en",
-        "pt": "pt_BR"
-    }
-    
-    let localizedText = ""
+  const cmsLangs: { [cmsLang: string]: string } =
+  {
+    "en": "en",
+    "pt": "pt_BR"
+  }
 
-    for(let item of localizedArray)
-    {
-        if(item.locale === cmsLangs[lng])
-            localizedText = item.value
-    }
+  let localizedText = ""
 
-    return localizedText
+  for (let item of localizedArray)
+  {
+    if (item.locale === cmsLangs[lng])
+      localizedText = item.value
+  }
+
+  return localizedText
 }
 
 export async function cmsService(query: string)
 {
   // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#revalidating-data
   // Check if should revalidate to get new content
-    const options = 
+  const options =
+  {
+    method: 'POST',
+    headers:
     {
-        method: 'POST',
-        headers: 
-        {
-            'Content-type': 'application/json',
-            'Authorization': 'Bearer ' + process.env.DATO_CMS_API_TOKEN
-        },
-        body: JSON.stringify
-        ({
-            query
-        })
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.DATO_CMS_API_TOKEN
+    },
+    body: JSON.stringify
+      ({
+        query
+      }),
+    next:
+    {
+      revalidate: 360
     }
-    const contentResponse = await fetch('https://graphql.datocms.com/', options)
+  }
+  const contentResponse = await fetch('https://graphql.datocms.com/', options)
     .then
     (
-        async (response) => 
-            {
-                const content = await response.json()
-                return content
-            }
+      async (response) => 
+      {
+        const content = await response.json()
+        return content
+      }
     )
-    return contentResponse.data
+  return contentResponse.data
 }
 
 export async function cmsQueryProjectSummaries()
 {
-    const contentQuery =`
+  const contentQuery = `
     query 
     {
       allCategoryProjects
@@ -87,13 +91,13 @@ export async function cmsQueryProjectSummaries()
         }
       }
     }`
-    const data: CMSResultCategory = await cmsService(contentQuery)
-    return data
+  const data: CMSResultCategory = await cmsService(contentQuery)
+  return data
 }
 
 export async function cmsQueryProjectById(id: string)
 {
-    const contentQuery =`
+  const contentQuery = `
     query 
     {
         contentProject(filter: {projectId: {eq: "${id}"}})
@@ -118,6 +122,6 @@ export async function cmsQueryProjectById(id: string)
           }
         }
       }`
-    const data: CMSResultProject = await cmsService(contentQuery)
-    return data
+  const data: CMSResultProject = await cmsService(contentQuery)
+  return data
 }
